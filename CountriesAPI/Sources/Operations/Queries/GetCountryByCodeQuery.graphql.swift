e6f025ea1fe255209e21ca809ca2e51c1dd852e6
Eduardo Hernandez <eduardohdzch@gmail.com>
2023-06-08 07:23:55 -0600

    Initial and final commit

---
+++ CountriesAPI/Sources/Operations/Queries/GetCountryByCodeQuery.graphql.swift
@@ -0,0 +1,89 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+@_exported import ApolloAPI
+
+public class GetCountryByCodeQuery: GraphQLQuery {
+  public static let operationName: String = "GetCountryByCode"
+  public static let document: ApolloAPI.DocumentType = .notPersisted(
+    definition: .init(
+      #"""
+      query GetCountryByCode($code: ID!) {
+        country(code: $code) {
+          __typename
+          code
+          name
+          capital
+          emoji
+          states {
+            __typename
+            name
+            code
+          }
+        }
+      }
+      """#
+    ))
+
+  public var code: ID
+
+  public init(code: ID) {
+    self.code = code
+  }
+
+  public var __variables: Variables? { ["code": code] }
+
+  public struct Data: CountriesAPI.SelectionSet {
+    public let __data: DataDict
+    public init(_dataDict: DataDict) { __data = _dataDict }
+
+    public static var __parentType: ApolloAPI.ParentType { CountriesAPI.Objects.Query }
+    public static var __selections: [ApolloAPI.Selection] { [
+      .field("country", Country?.self, arguments: ["code": .variable("code")]),
+    ] }
+
+    public var country: Country? { __data["country"] }
+
+    /// Country
+    ///
+    /// Parent Type: `Country`
+    public struct Country: CountriesAPI.SelectionSet {
+      public let __data: DataDict
+      public init(_dataDict: DataDict) { __data = _dataDict }
+
+      public static var __parentType: ApolloAPI.ParentType { CountriesAPI.Objects.Country }
+      public static var __selections: [ApolloAPI.Selection] { [
+        .field("__typename", String.self),
+        .field("code", CountriesAPI.ID.self),
+        .field("name", String.self),
+        .field("capital", String?.self),
+        .field("emoji", String.self),
+        .field("states", [State].self),
+      ] }
+
+      public var code: CountriesAPI.ID { __data["code"] }
+      public var name: String { __data["name"] }
+      public var capital: String? { __data["capital"] }
+      public var emoji: String { __data["emoji"] }
+      public var states: [State] { __data["states"] }
+
+      /// Country.State
+      ///
+      /// Parent Type: `State`
+      public struct State: CountriesAPI.SelectionSet {
+        public let __data: DataDict
+        public init(_dataDict: DataDict) { __data = _dataDict }
+
+        public static var __parentType: ApolloAPI.ParentType { CountriesAPI.Objects.State }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("__typename", String.self),
+          .field("name", String.self),
+          .field("code", String?.self),
+        ] }
+
+        public var name: String { __data["name"] }
+        public var code: String? { __data["code"] }
+      }
+    }
+  }
+}
